7b8e414b9a402797e2c156ddb731afba91c6f6e8
Cyril Limam <cl@booqee.com>
2018-04-18 20:37:47 +0200

    Fixes: *
    Changes:
    * setRf is not a const function

--- modules/ximgproc/src/structured_edge_detection.cpp
+++ modules/ximgproc/src/structured_edge_detection.cpp
@@ -669,7 +669,7 @@ public:
      * RandomForest setter
      */
 
-    void setRf(cv::ximgproc::RandomForest_t rf) const CV_OVERRIDE
+    void setRf(cv::ximgproc::RandomForest_t rf) CV_OVERRIDE
     {
         __rf = rf;
     }
