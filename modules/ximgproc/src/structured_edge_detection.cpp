eb023623a2a0caec8bf7f0b6adb1829578b5c34b
Younn Olivier <yo@booqee.com>
2018-04-18 12:46:27 +0200

    RandomForest struct made public

--- modules/ximgproc/src/structured_edge_detection.cpp
+++ modules/ximgproc/src/structured_edge_detection.cpp
@@ -656,6 +656,55 @@ public:
       dst.copyTo(_dst);
     }
 
+    /*! random forest used to detect edges */
+    struct RandomForest
+    {
+        /*! random forest options, e.g. number of trees */
+        struct RandomForestOptions
+        {
+            // model params
+
+            int numberOfOutputChannels; /*!< number of edge orientation bins for output */
+
+            int patchSize;              /*!< width of image patches */
+            int patchInnerSize;         /*!< width of predicted part inside patch*/
+
+            // feature params
+
+            int regFeatureSmoothingRadius;    /*!< radius for smoothing of regular features
+                                               *   (using convolution with triangle filter) */
+
+            int ssFeatureSmoothingRadius;     /*!< radius for smoothing of additional features
+                                               *   (using convolution with triangle filter) */
+
+            int shrinkNumber;                 /*!< amount to shrink channels */
+
+            int numberOfGradientOrientations; /*!< number of orientations per gradient scale */
+
+            int gradientSmoothingRadius;      /*!< radius for smoothing of gradients
+                                               *   (using convolution with triangle filter) */
+
+            int gradientNormalizationRadius;  /*!< gradient normalization radius */
+            int selfsimilarityGridSize;       /*!< number of self similarity cells */
+
+            // detection params
+            int numberOfTrees;            /*!< number of trees in forest to train */
+            int numberOfTreesToEvaluate;  /*!< number of trees to evaluate per location */
+
+            int stride;                   /*!< stride at which to compute edges */
+
+        } options;
+
+        int numberOfTreeNodes;
+
+        std::vector <int> featureIds;     /*!< feature coordinate thresholded at k-th node */
+        std::vector <float> thresholds;   /*!< threshold applied to featureIds[k] at k-th node */
+        std::vector <int> childs;         /*!< k --> child[k] - 1, child[k] */
+
+        std::vector <int> edgeBoundaries; /*!< ... */
+        std::vector <int> edgeBins;       /*!< ... */
+    }
+
     /*!
      * RandomForest getter
      */
@@ -863,54 +912,7 @@ protected:
     /*! optional feature getter (getFeatures method) */
     Ptr<const RFFeatureGetter> howToGetFeatures;
 
-    /*! random forest used to detect edges */
-    struct RandomForest
-    {
-        /*! random forest options, e.g. number of trees */
-        struct RandomForestOptions
-        {
-            // model params
-
-            int numberOfOutputChannels; /*!< number of edge orientation bins for output */
-
-            int patchSize;              /*!< width of image patches */
-            int patchInnerSize;         /*!< width of predicted part inside patch*/
-
-            // feature params
-
-            int regFeatureSmoothingRadius;    /*!< radius for smoothing of regular features
-                                               *   (using convolution with triangle filter) */
-
-            int ssFeatureSmoothingRadius;     /*!< radius for smoothing of additional features
-                                               *   (using convolution with triangle filter) */
-
-            int shrinkNumber;                 /*!< amount to shrink channels */
-
-            int numberOfGradientOrientations; /*!< number of orientations per gradient scale */
-
-            int gradientSmoothingRadius;      /*!< radius for smoothing of gradients
-                                               *   (using convolution with triangle filter) */
-
-            int gradientNormalizationRadius;  /*!< gradient normalization radius */
-            int selfsimilarityGridSize;       /*!< number of self similarity cells */
-
-            // detection params
-            int numberOfTrees;            /*!< number of trees in forest to train */
-            int numberOfTreesToEvaluate;  /*!< number of trees to evaluate per location */
-
-            int stride;                   /*!< stride at which to compute edges */
-
-        } options;
-
-        int numberOfTreeNodes;
-
-        std::vector <int> featureIds;     /*!< feature coordinate thresholded at k-th node */
-        std::vector <float> thresholds;   /*!< threshold applied to featureIds[k] at k-th node */
-        std::vector <int> childs;         /*!< k --> child[k] - 1, child[k] */
-
-        std::vector <int> edgeBoundaries; /*!< ... */
-        std::vector <int> edgeBins;       /*!< ... */
-    } __rf;
+    RandomForest __rf;
 };
 
 Ptr<StructuredEdgeDetection> createStructuredEdgeDetection(const String &model,
